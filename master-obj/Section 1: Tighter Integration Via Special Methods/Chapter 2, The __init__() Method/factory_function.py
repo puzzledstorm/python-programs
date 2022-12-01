937c04485141e8c7a178362ce4dfe3bc5efe1491
女孩子都好可爱 <74460276+puzzledstorm@users.noreply.github.com>
2022-12-01 13:42:31 +0000

    push by codespace

--- master-obj/Section 1: Tighter Integration Via Special Methods/Chapter 2, The __init__() Method/factory_function.py
+++ master-obj/Section 1: Tighter Integration Via Special Methods/Chapter 2, The __init__() Method/factory_function.py
@@ -14,3 +14,107 @@ strengths of Python is that we're not forced to use a class hierarchy when a
 simple function might do just as well
 """
 
+
+# from https://github.com/PacktPublishing/Mastering-Object-Oriented-Python-Second-Edition/blob/master/Chapter_2/ch02_ex2.py
+# ---------------------------------------------
+
+from typing import Tuple, Any, Union, cast
+
+class Card:
+    insure = False
+
+    def __init__(self, rank: str, suit: Any) -> None:
+        self.suit = suit
+        self.rank = rank
+        self.hard, self.soft = self._points()
+
+    def __eq__(self, other: Any) -> bool:
+        return (
+            self.suit == cast("Card", other).suit
+            and self.rank == cast("Card", other).rank
+            and self.hard == cast("Card", other).hard
+            and self.soft == cast("Card", other).soft
+        )
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(suit={self.suit!r}, rank={self.rank!r})"
+
+    def __str__(self) -> str:
+        return f"{self.rank}{self.suit}"
+
+    def _points(self) -> Tuple[int, int]:
+        return int(self.rank), int(self.rank)
+
+
+class AceCard(Card):
+    insure = True
+
+    def _points(self) -> Tuple[int, int]:
+        return 1, 11
+
+
+class FaceCard(Card):
+
+    def _points(self) -> Tuple[int, int]:
+        return 10, 10
+
+from enum import Enum
+
+
+class Suit(str, Enum):
+    Club = "♣"
+    Diamond = "♦"
+    Heart = "♥"
+    Spade = "♠"
+
+# -------------------------------------------
+from typing import cast, Iterable, Iterator
+
+# 1.factory function
+def card(rank: int, suit: Suit) -> Card:
+    if rank == 1:
+        return AceCard("A", suit)
+    elif 2 <= rank < 11:
+        return Card(str(rank), suit)
+    elif 11 <= rank < 14:
+        name = {11: "J", 12: "Q", 13: "K"}[rank]
+        return FaceCard(name, suit)
+    raise Exception("Design Failure")
+
+deck = [
+    card(rank, suit) for rank in range(1, 14) for suit in cast(Iterable[Suit], Suit)
+]
+deck_l = [
+    card(rank, suit) for rank in range(1, 14) for suit in iter(Suit)
+]
+
+deck_l = [
+    card(rank, suit) for rank in range(1, 14) for suit in list(Suit)
+]
+
+
+deck_l = [
+    card(rank, suit) for rank in range(1, 14) for suit in Suit
+] # mypy没有报错signal errors啊
+
+# print(deck)
+# print(deck_l)
+
+
+# It's important to avoid a vague else clause.
+
+# 使用不明确的else示例
+def card2(rank: int, suit: Suit) -> Card:
+    if rank == 1:
+        return AceCard("A", suit)
+    elif 2 <= rank < 11:
+        return Card(str(rank), suit)
+    else:
+        name = {11: "J", 12: "Q", 13: "K"}[rank]
+        return FaceCard(name, suit)
+deck2 = [card2(rank, suit) for rank in range(13) for suit in Suit]
+
+
+# Factory design patterns:
+# An if-elif sequence
+# A mapping
